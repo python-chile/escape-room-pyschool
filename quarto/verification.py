118c9248f205b7324d77e20303c1be7231a3f40f
Sebastian Flores <sebastian.flores@u-planner.com>
2025-04-04 23:05:14 -0300

    more testing

--- quarto/verification.py
+++ quarto/verification.py
@@ -1,8 +1,11 @@
+# types: alert, danger, success, info, primary
+
 def hyperlink(url, text, type="info"):
     print(f'<a href="{url}" class="btn btn-{type}">{text}</a>')
 
 
 def text(title="", text="", type="info", dismissable=True):
+    """
     class_str = f'alert alert-{type}'
     if dismissable: class_str += " alert-dismissible"
     text_str = '<div class="{class_str}">'
@@ -10,6 +13,13 @@ def text(title="", text="", type="info", dismissable=True):
     if text: text_str += f'<p class=\"mb-0\">{text}</p>'
     text_str += '</div>'
     print(text_str)
+    """
+    text_str = '''
+    <div class="alert alert-dismissible alert-danger">
+    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+    </div>    
+    '''
 
 
 def check_answer(answer, room_number):
@@ -31,18 +41,16 @@ def check_answer(answer, room_number):
 
 def check_answer_1(answer):
     if answer == 5:
-        hyperlink("2.html", "¡Correcto! Avanza a la siguiente página")
+        hyperlink("2.html", "¡Correcto! Avanza a la siguiente página", "success")
     else:
-        text("No no no...", "intenta de nuevo.", "danger")
+        text("No no no...", "intenta de nuevo.", "warning")
 
 
 def check_answer_2(answer):
     if answer == "2":
-        hyperlink("end.html", "¡Correcto! Avanza a la siguiente página")
+        hyperlink("end.html", "¡Correcto! Avanza a la siguiente página", "success")
     else:
-        text("No no no...", "intenta de nuevo.", "danger")
-
-print("OK")
+        text("No no no...", "intenta de nuevo.", "warning")
 
 if __name__ == "__main__":
     check_answer(5, 1)
