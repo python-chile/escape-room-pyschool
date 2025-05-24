cfe7a8556940efd76a140477233dd0de90063e50
Tony-Rome <tonyn.rome@gmail.com>
2025-05-24 12:13:54 -0400

    Fix room 03

--- quarto/rooms/sala_03.py
+++ quarto/rooms/sala_03.py
@@ -2,7 +2,7 @@ from helpers import text, hyperlink
 
 def revisar(answer):
     # \frac{1.23 + 2.34}{1 + 43/2} + 3 \times 2^{1.5}
-    true_answer = (1.23 + 2.34) / (1 + 43 / 2) + 3 * 2 ** 1.5
+    true_answer = ( (1.23 + 2.34) / (1 + (43 / 2) ) ) + (3 * 2**1.5)
     epsilon = 0.000001
     if type(answer) in [float, int] and abs(answer - true_answer) < epsilon:
         hyperlink("¡Correcto! Haz click aquí para avanzar al siguiente desafío", "sala_04.html", "success")
