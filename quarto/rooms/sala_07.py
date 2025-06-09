35b61e9182b881e9cacfbbe7b49eccd9229648ca
Tony-Rome <tonyn.rome@gmail.com>
2025-06-09 18:57:19 -0400

    Update script 7

--- quarto/rooms/sala_07.py
+++ quarto/rooms/sala_07.py
@@ -3,9 +3,7 @@ from helpers import text, hyperlink
 def revisar(respuesta):
     lista = ["GRANDE_10", "GRANDE_1", "GRANDE_3"]
 
-    if respuesta is None:
-        text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == lista:
+    if respuesta == lista:
         hyperlink("¡Correcto! Avanza a la siguiente página", "sala_08.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
