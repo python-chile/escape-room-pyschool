c54389c81c377d571cca8939e05c0c6a4302ae2a
Tony-Rome <tonyn.rome@gmail.com>
2025-06-08 21:32:44 -0400

    Room 7 script

--- quarto/rooms/sala_07.py
+++ quarto/rooms/sala_07.py
@@ -1,14 +1,16 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
-    if respuesta == None:
+    lista = ["GRANDE_10", "GRANDE_1", "GRANDE_3"]
+    if respuesta == None or type(respuesta) != list:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == lista:
+        hyperlink("¡Correcto! Avanza a la siguiente página", "sala_08.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
 if __name__ == "__main__":
-    revisar(None)
-    revisar(True)
-    revisar(False)
+    objetos = ["MEDIANO_3", "MEDIANO_1", "MEDIANO_3", "GRANDE_1", "PEQUEÑO_10", "GRANDE_10", "GRANDE_3", "PEQUEÑO_1", "PEQUEÑO_10"]
+    print(sorted(objetos, reverse=True))
+    lista = ["GRANDE_10", "GRANDE_1", "GRANDE_3"]
+    print(lista)
