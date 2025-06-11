7e1014611abed1596838ac87c7e3394024f953ad
Tony-Rome <tonyn.rome@gmail.com>
2025-06-10 20:35:31 -0400

    Script room 9

--- quarto/rooms/sala_09.py
+++ quarto/rooms/sala_09.py
@@ -1,14 +1,14 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
-    if respuesta == None:
-        text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    lista = ['linterna', 'monitor principal', 'antena', 'filtro gases', 'lentes infrarojo', 'teclado']
+    if respuesta == lista:
+        hyperlink("¡Correcto! Avanza a la siguiente página", "sala_10.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
 if __name__ == "__main__":
+    lista = ['linterna', 'monitor principal', 'antena', 'filtro gases', 'lentes infrarojo', 'teclado']
     revisar(None)
-    revisar(True)
-    revisar(False)
+    revisar("lista")
+    revisar(lista)
