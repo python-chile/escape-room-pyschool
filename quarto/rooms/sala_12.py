c5840867355ee9b3a75c2c444c19a77b633b257e
Tony-Rome <tonyn.rome@gmail.com>
2025-07-09 14:19:10 -0400

    feat challenge review

--- quarto/rooms/sala_12.py
+++ quarto/rooms/sala_12.py
@@ -1,12 +1,16 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
+
+    diccionario = {'PS004': 80, 'PS014': 120, 'PS104': 50}
+
     if respuesta == None:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == diccionario:
+        hyperlink("¡Correcto! Haz click aquí para avanzar al siguiente desafío", "sala_13.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
+
 
 if __name__ == "__main__":
     revisar(None)
