a6a9d39f2bc42ae86e578da0cc84f1ccced17a54
Tony-Rome <tonyn.rome@gmail.com>
2025-07-09 13:41:34 -0400

    feat: room 11

--- quarto/rooms/sala_11.py
+++ quarto/rooms/sala_11.py
@@ -1,14 +1,24 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
+
+    palabras_unidas = unir_palabras(["Misión", "PySchool 2025", "progreso a mitad", "con pequeñas dificultades", "pronto", "nuevo", "reporte"])
+
     if respuesta == None:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == palabras_unidas:
+        hyperlink("¡Correcto! Haz click aquí para avanzar al siguiente desafío", "sala_12.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
+def unir_palabras(palabras):
+    palabras_unidas = " ".join(palabras)
+    return palabras_unidas
+
 if __name__ == "__main__":
+    palabras= ["Misión", "PySchool 2025", "progreso a mitad", "con pequeñas dificultades", "pronto", "nuevo", "reporte"]
+    palabras_unidas = " ".join(palabras)
+
     revisar(None)
-    revisar(True)
+    revisar(palabras_unidas)
     revisar(False)
