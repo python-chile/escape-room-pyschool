c80ca0ce41646403fad77c4071f8cb35fb1badf6
Tony-Rome <tonyn.rome@gmail.com>
2025-07-10 23:05:57 -0400

    feat: room 17

--- quarto/rooms/sala_17.py
+++ quarto/rooms/sala_17.py
@@ -1,14 +1,36 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
+    codigo_salida = 963
+
     if respuesta == None:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == codigo_salida:
+        hyperlink("¡Correcto! Haz click aquí para despegar de vuelta a la Tierra", "sala_18.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
+def es_primo(numero):
+
+    if numero <= 3: return True
+    else:
+        for i in range(2, numero - 1):
+            if numero % i == 0: return False
+        return True
+
+def suma_numeros_primos(codigo_secreto):
+    suma = 0
+    numero = 2
+    contador = 0
+    for _ in range(1, 1000):
+        if es_primo(numero):
+            suma += numero
+            contador += 1
+        numero += 1
+        if contador == codigo_secreto: break
+    return suma
+
 if __name__ == "__main__":
     revisar(None)
-    revisar(True)
+    revisar(suma_numeros_primos(24))
     revisar(False)
