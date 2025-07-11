de435ec344c749a3ca8caab1c86093c9791a2ea7
Tony-Rome <tonyn.rome@gmail.com>
2025-07-10 21:41:13 -0400

    feat: room 14

--- quarto/rooms/sala_14.py
+++ quarto/rooms/sala_14.py
@@ -1,14 +1,51 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
+
+    mensaje = "el codigo secreto que se debe ocupar en el ultimo desafio es 24"
+
     if respuesta == None:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == mensaje:
+        hyperlink("¡Correcto! Haz click aquí para avanzar al siguiente desafío", "sala_15.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
+
+def mensaje_decodificado(mensaje):
+    diccionario = {
+        '._': 'a', '_...': 'b', '_._.': 'c', '_..': 'd',
+        '.': 'e', '.._.': 'f', '__.': 'g', '....': 'h',
+        '..': 'i', '.___': 'j', '_._': 'k', '._..': 'l',
+        '__': 'm', '_.': 'n', '___': 'o', '.__.': 'p',
+        '__._': 'q', '._.': 'r', '...': 's', '_': 't',
+        '.._': 'u', '..._': 'v', '.__': 'w', '_.._': 'x',
+        '_.__': 'y', '__..': 'z', '__._.': ' ', '_____': '0',
+        '.____': '1', '..___': '2', '...__': '3', '...._': '4',
+        '.....': '5', '_....': '6', '__...': '7', '___..': '8',
+        '____.': '9'
+    }
+
+    decodificado = [diccionario[simbolo] for simbolo in mensaje]
+    return ''.join(decodificado)
+
 if __name__ == "__main__":
+    mensaje = [
+        '.', '._..', '__._.',  # e l
+        '_._.', '___', '_..', '..', '__.', '___', '__._.', # c o d i g o
+        '...', '.', '_._.', '._.', '.', '_', '___', '__._.', # s e c r e t o
+        '__._', '.._', '.', '__._.',  # q u e
+        '...', '.', '__._.', # s e
+        '_..', '.', '_...', '.', '__._.', # d e b e
+        '___', '_._.', '.._', '.__.', '._', '._.', '__._.', # o c u p a r
+        '.', '_.', '__._.', # e n
+        '.', '._..', '__._.', # e l
+        '.._', '._..', '_', '..', '__', '___', '__._.', # u l t i m o
+        '_..', '.', '...', '._', '.._.', '..', '___', '__._.', # d e s a f i o
+        '.', '...', '__._.', # e s
+        '..___', '...._'  # 2 4
+    ]
+
     revisar(None)
+    revisar(mensaje_decodificado(mensaje))
     revisar(True)
-    revisar(False)
