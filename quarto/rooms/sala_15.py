a29d6b806adb9276dc9506419155d5311e42bfe6
Tony-Rome <tonyn.rome@gmail.com>
2025-07-15 15:39:42 -0400

    feat: room 15 code text

--- quarto/rooms/sala_15.py
+++ quarto/rooms/sala_15.py
@@ -1,14 +1,29 @@
 from helpers import text, hyperlink
 
 def revisar(respuesta):
+
+    lista = [(20, 30), (21, 29), (22, 28), (23, 27), (24, 26), (25, 25), (26, 24), (27, 23), (28, 22), (29, 21), (30, 20)]
     if respuesta == None:
         text("Intenta cambiar el valor de la variable `respuesta`.", "info")
-    elif respuesta == True:
-        hyperlink("¡Correcto! Avanza a la siguiente página", "2.html", "success")
+    elif respuesta == lista:
+        hyperlink("¡Correcto! Haz click aquí para avanzar al siguiente desafío", "sala_16.html", "success")
     else:
         text("No es la respuesta correcta. Inténtalo nuevamente.", "warning")
 
+
+def combinaciones():
+    coordenadas = list(range(1, 31))
+    combinaciones_validas = []
+
+    for x in coordenadas:
+        for y in coordenadas:
+            if x + y == 50:
+                combinaciones_validas.append((x, y))
+
+    return combinaciones_validas
+
+
 if __name__ == "__main__":
     revisar(None)
-    revisar(True)
+    revisar(combinaciones())
     revisar(False)
